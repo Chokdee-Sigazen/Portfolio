8e30c3bdb3ad57e851fa2f1a58985a1166bf5751
Sigazen <choikdee50@gmail.com>
2023-11-23 23:51:22 +0700

    Add Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,3 @@
-import "./App.css";
-
-function App() {
-  return (
-    <>
-      <div className=" bg-black text-yellow-800">Hello World</div>
-      <div>Hello World</div>
-    </>
-  );
+export default function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
-
-export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /*eslint-env node*/
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
